c310e8e7847a3acdb157a460fcc25fc4f9da8418
kkumlien <kkumlien@users.noreply.github.com>
2017-11-06 13:31:37 +0000

    #26 Added database schema diagram and DDL scripts.

---
+++ resources/assets/database/PoMoodle_mysql_create.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `users` (
+	`user_id` varchar(30) NOT NULL UNIQUE,
+	`site_id` INT NOT NULL UNIQUE,
+	`role_code` varchar(1) NOT NULL
+);
+
+CREATE TABLE `sites` (
+	`site_id` INT NOT NULL AUTO_INCREMENT,
+	`site_url` varchar(100) NOT NULL UNIQUE,
+	PRIMARY KEY (`site_id`)
+);
+
+CREATE TABLE `modules` (
+	`module_id` INT NOT NULL,
+	`course_id` INT NOT NULL,
+	`module_name` varchar(50) NOT NULL,
+	PRIMARY KEY (`module_id`)
+);
+
+CREATE TABLE `activities` (
+	`activity_id` INT NOT NULL AUTO_INCREMENT,
+	`user_id` INT NOT NULL,
+	`module_id` INT NOT NULL,
+	`activity_name` varchar(50) NOT NULL,
+	`completed_flag` BOOLEAN NOT NULL,
+	`duration_in_minutes` INT NOT NULL,
+	`completed_timestamp` TIMESTAMP,
+	PRIMARY KEY (`activity_id`)
+);
+
+CREATE TABLE `courses` (
+	`course_id` INT NOT NULL,
+	`site_id` INT NOT NULL,
+	`teacher_user_id` INT NOT NULL,
+	`course_name` varchar(50) NOT NULL,
+	PRIMARY KEY (`course_id`)
+);
+
+ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`site_id`) REFERENCES `sites`(`site_id`);
+
+ALTER TABLE `modules` ADD CONSTRAINT `modules_fk0` FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`);
+
+ALTER TABLE `activities` ADD CONSTRAINT `activities_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
+
+ALTER TABLE `activities` ADD CONSTRAINT `activities_fk1` FOREIGN KEY (`module_id`) REFERENCES `modules`(`module_id`);
+
+ALTER TABLE `courses` ADD CONSTRAINT `courses_fk0` FOREIGN KEY (`site_id`) REFERENCES `sites`(`site_id`);
+
+ALTER TABLE `courses` ADD CONSTRAINT `courses_fk1` FOREIGN KEY (`teacher_user_id`) REFERENCES `users`(`user_id`);
+
