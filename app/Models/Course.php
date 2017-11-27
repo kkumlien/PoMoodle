fe39b16f08ce58f3c99fd1ebfc1cec3c3324a296
leonardoaml <leonardo_aml@hotmail.com>
2017-11-27 20:20:24 +0000

    #62 Student Homepage layer commit. Added Course Model

---
+++ app/Models/Course.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Models;
+
+
+class Course
+{
+    /**
+     * @var int
+     */
+    public $id;
+    /**
+     * @var string
+     */
+    public $fullname;
+    /**
+     * @var string
+     */
+    public $shortname;
+    /**
+     * @var int
+     */
+    public $visible;
+    /**
+     * @var string
+     */
+    public $summary;
+    /**
+     * @var int
+     */
+    public $category;
+    /**
+     * @var int
+     */
+    public $progress;
+    /**
+     * @var int
+     */
+    public $startdate;
+    /**
+     * @var int
+     */
+    public $enddate;
+
+}
+
